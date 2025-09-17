7dde4d63524c7c31948e369e2553fcdff3f91e6b
sharmila <sharmilamohammad04@gmail.com>
2025-09-17 22:44:46 +0530

    Day-3

---
+++ Javacore/src/com/tnsif/basics/prime1to100.java
@@ -0,0 +1,22 @@
+package com.tnsif.basics;
+public class prime1to100 {
+	public static void main(String[] args) {
+	        System.out.println("Prime numbers from 1 to 100 :");
+	        for (int i = 2; i <= 100; i++) {
+	            if (isPrime(i)) {
+	                System.out.println(i);
+	            }
+	        }
+	    }
+	    public static boolean isPrime(int num) {
+	        if (num < 2) {
+	            return false;
+	        }
+	        for (int i = 2; i <= Math.sqrt(num); i++) {
+	            if (num % i == 0) {
+	                return false;
+	            }
+	        }
+	        return true;
+	    }
+	}
