0a2a2339031832e8f9a6ca692a4339b39b808cf3
sharmila <sharmilamohammad04@gmail.com>
2025-09-24 23:03:19 +0530

    Task-7

--- Javacore/src/com/tnsif/exception/tryCatch.java
+++ Javacore/src/com/tnsif/exception/tryCatch.java
@@ -1,4 +1,12 @@
 package com.tnsif.exception;
 public class tryCatch {
-           
-}
+	    public static void main(String[] args) {
+	        try {
+	            int a = 10, b = 0;
+	            int c = a / b; 
+	            System.out.println("Result: " + c);
+	        } catch (ArithmeticException e) {
+	            System.out.println("Error: Division by zero is not allowed!");
+	        }
+	    }
+	}
