4f688f931cf8a2ddf33c55dc252d9fb0f8237f9e
sharmila <sharmilamohammad04@gmail.com>
2025-10-08 23:19:51 +0530

    JDBC

---
+++ JDBCprgrms/src/jdbcprgrms/Demo.java
@@ -0,0 +1,23 @@
+package jdbcprgrms;
+import java.sql.*;
+import javax.sql.*;
+public class Demo {
+	public static void main(String[] args) throws Exception{
+		String URL = "jdbc:mysql://localhost:3306/tnsifjava1";
+		String USER = "root";
+		String PASSWORD = "root@123";
+		//step-1 : load and register the driver
+		Class.forName("com.mysql.cj.jdbc.Driver");	
+		System.out.println("The Driver is loaded successfully");
+		//step-2 : Establishing the connection
+		Connection con =  DriverManager.getConnection(URL,USER,PASSWORD);
+		System.out.println("Connection Established");
+		//step-3 : statement creation
+		Statement st = con.createStatement();	
+		//step-4 : execute a query
+		st.executeUpdate("CREATE TABLE studentinform(sid int,sname varchar(20))");
+		System.out.println("Table created");	
+		st.close();
+		con.close();		
+	}
+}
---
+++ JDBCprgrms/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module JDBCprgrms {
+	requires jdk.jdi;
+	requires java.sql;
+}
